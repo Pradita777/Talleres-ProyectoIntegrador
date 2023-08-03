f681746ffbb56706fb8b5ecfb774e7a919fa479c
Pradita777 <andres123prada@gmail.com>
2023-08-03 17:45:31 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'name':'Greg Lim'})
+    return render(request, 'home.html',{'name':'Andrés Prada Rodriguez'})
